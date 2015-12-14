b5e0fce34d5f64e9468da0b396bfbd612b70a617
azuer88 <blue.cuenca@gmail.com>
2015-12-14 09:25:38 +0800

    fix cover page image sizing

--- src/ebook.py
+++ src/ebook.py
@@ -2,16 +2,19 @@
 """
 epub object to create an epub ebook
 """
+
 from lxml import etree as ET
-#import lxml.builder as builder
+# import lxml.builder as builder
 from lxml import html as HT
-#from lxml.etree import Element
+# from lxml.etree import Element
 import os
 import zipfile
 
-import htmlentitydefs, re
+import htmlentitydefs
+import re
 
-#import logging
+# import logging
+
 
 def valid_filename(strname):
     """
@@ -22,19 +25,22 @@ def valid_filename(strname):
 
     fname_only, fext_only = os.path.splitext(strname)
     valid_chars = "-_() %s%s" % (string.ascii_letters, string.digits)
-    #valid_chars = "-_.() %s%s" % (string.ascii_letters, string.digits)
+
+    # valid_chars = "-_.() %s%s" % (string.ascii_letters, string.digits)
     valid_fname = ''.join(c for c in fname_only if c in valid_chars)
     return valid_fname + fext_only
+
 
 def sluggify(text, separator="-"):
     """
     sluggify text
     """
+
     ret = ""
     for char in text.lower():
         try:
             ret += htmlentitydefs.codepoint2name[ord(char)]
-        except: # pylint: disable-msg=W0702
+        except:  # pylint: disable-msg=W0702
             ret += char
 
     ret = re.sub(r"([a-zA-Z])(uml|acute|grave|circ|tilde|cedil)", r"\1", ret)
@@ -43,16 +49,20 @@ def sluggify(text, separator="-"):
     ret = re.sub(r":", r">", ret)
     return ret.strip()
 
+
 class ContainerXML(object):
+
     """
     encapsulate the XML Container structure
     """
+
     def __init__(self):
         self.encoding = 'utf-8'
         self.namespace = "urn:oasis:names:tc:opendocument:xmlns:container"
         self.version = "1.0"
         self.media_type = "application/oebps-package+xml"
         self.full_path = "OEBPS/content.opf"
+
     def build(self):
         """
         build an XML container structure
@@ -61,30 +71,36 @@ class ContainerXML(object):
                           version=self.version)
         rfs = ET.SubElement(root, "rootfiles")
         attrs = {"full-path": self.full_path, "media-type": self.media_type, }
-        dummy = ET.SubElement(rfs, # pylint: disable-msg=W0142
+        dummy = ET.SubElement(rfs,  # pragma pylint: disable=W0612
                               "rootfile", **attrs)
+        # pragma pylint: enable=W0612
         return root
+
     def dummy(self):
         """
         dummy method
         """
         pass
+
     def __repr__(self):
         root = self.build()
         return ET.tostring(root, pretty_print=True, xml_declaration=True,
                            encoding=self.encoding)
 
-class ContainerOPF(object): # pylint: disable=too-many-instance-attributes
+
+class ContainerOPF(object):  # pylint: disable=too-many-instance-attributes
+
     """
     encapsulte OPF Container structure
     """
+
     def __init__(self):
         self.encoding = "utf-8"
         self.namespace_opf = "http://www.idpf.org/2007/opf"
         self.namespace_dc = "http://purl.org/dc/elements/1.1/"
         self.namespace_map = {
-            None : self.namespace_opf,
-            "dc" : self.namespace_dc,
+            None: self.namespace_opf,
+            "dc": self.namespace_dc,
             }
         self.attr = {
             "xmlns": self.namespace_opf,
@@ -148,7 +164,7 @@ class ContainerOPF(object): # pylint: disable=too-many-instance-attributes
         manifest = ET.SubElement(root, "manifest")
         for sid, href, media_type in self.manifest:
             args = {"id": sid, "href": href, "media-type": media_type}
-            ET.SubElement(manifest, "item", **args) # pylint: disable-msg=W0142
+            ET.SubElement(manifest, "item", **args)  # pylint: disable-msg=W0142
 
     def build_spine(self, root):
         """
@@ -157,7 +173,7 @@ class ContainerOPF(object): # pylint: disable=too-many-instance-attributes
         spine = ET.SubElement(root, "spine", toc="ncx")
         for idref, linear in self.spine:
             args = {"idref": idref, "linear": linear}
-            ET.SubElement(spine, "itemref", **args) # pylint: disable-msg=W0142
+            ET.SubElement(spine, "itemref", **args)  # pylint: disable-msg=W0142
 
     def _test(self):
         """
@@ -178,11 +194,14 @@ class ContainerOPF(object): # pylint: disable=too-many-instance-attributes
         return ET.tostring(root, xml_declaration=True,
                            pretty_print=True, encoding=self.encoding)
 
-class XHTMLFile(object): # pylint: disable=too-many-instance-attributes
+
+class XHTMLFile(object):  # pylint: disable=too-many-instance-attributes
+
     """
     encapsulate an XTMLFile, eases the creation of a suitable xhtml file for
     epub consumption
     """
+
     def __init__(self):
         self.doctype = """<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
     "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">"""
@@ -228,10 +247,10 @@ class XHTMLFile(object): # pylint: disable=too-many-instance-attributes
         head = ET.SubElement(root, "head")
         for key, val, attr in self.headers:
             if val:
-                ET.SubElement(head, # pylint: disable-msg=W0142
+                ET.SubElement(head,  # pylint: disable-msg=W0142
                               key, **attr).text = val
             else:
-                ET.SubElement(head, key, **attr) # pylint: disable-msg=W0142
+                ET.SubElement(head, key, **attr)  # pylint: disable-msg=W0142
         ET.SubElement(head, "title").text = self.title
 
     def build_body(self, root):
@@ -242,7 +261,7 @@ class XHTMLFile(object): # pylint: disable=too-many-instance-attributes
         body = ET.SubElement(root, "body")
         last = None
         for frag in fragments:
-            if isinstance(frag, ET._Element): # pylint: disable-msg=W0212
+            if isinstance(frag, ET._Element):  # pylint: disable-msg=W0212
                 body.append(frag)
                 last = frag
             else:
@@ -275,10 +294,13 @@ class XHTMLFile(object): # pylint: disable=too-many-instance-attributes
         with open(filename, "w") as output:
             output.write(self.__repr__())
 
+
 class TOCNCX(object):
+
     """
     oject to encapsulate a TOC/NCX structure
     """
+
     def __init__(self):
         self.doctype = """<!DOCTYPE ncx PUBLIC "-//NISO//DTD ncx 2005-1//EN"
                  "http://www.daisy.org/z3986/2005/ncx-2005-1.dtd">"""
@@ -296,21 +318,21 @@ class TOCNCX(object):
         root = ET.Element("ncx", xmlns=self.namespace, version=self.version)
         head = ET.SubElement(root, "head")
         ET.SubElement(head, "meta",
-            content="urn:uuid:%s" % self.bookid,
-            name="dtb:uid",
-            )
+                      content="urn:uuid:%s" % self.bookid,
+                      name="dtb:uid",
+                      )
         ET.SubElement(head, "meta",
-            content="1",
-            name="dtb:depth",
-            )
+                      content="1",
+                      name="dtb:depth",
+                      )
         ET.SubElement(head, "meta",
-            content="0",
-            name="dtb:totalPageCount",
-            )
+                      content="0",
+                      name="dtb:totalPageCount",
+                      )
         ET.SubElement(head, "meta",
-            content="0",
-            name="dtb:maxPageNumber",
-            )
+                      content="0",
+                      name="dtb:maxPageNumber",
+                      )
         doctitle = ET.SubElement(root, "docTitle")
         ET.SubElement(doctitle, "text").text = self.title
         navmap = ET.SubElement(root, "navMap")
@@ -345,14 +367,17 @@ class TOCNCX(object):
         return ET.tostring(root, xml_declaration=True, pretty_print=True,
                            encoding=self.encoding, doctype=self.doctype)
 
-class EPub(object): # pylint: disable=too-many-instance-attributes
+
+class EPub(object):  # pylint: disable=too-many-instance-attributes
+
     """
     class to create epub ebook
     """
-    #OEBPS = 'OEBPS'
+    # OEBPS = 'OEBPS'
     TEXT = 'Text'
     IMAGES = 'Images'
     STYLES = 'Styles'
+
     def __init__(self, title, author, bookid, folder="", prefix=""):
         self.opf = ContainerOPF()
         self.xml = ContainerXML()
@@ -386,7 +411,9 @@ class EPub(object): # pylint: disable=too-many-instance-attributes
         """
         property filename of the ebook
         """
-        return valid_filename("%s%s by %s.epub" % (self.prefix, self.title, self.author))
+        return valid_filename(
+            "%s%s by %s.epub" %
+            (self.prefix, self.title, self.author))
 
     def close(self):
         """
@@ -455,12 +482,12 @@ class EPub(object): # pylint: disable=too-many-instance-attributes
         """
         store image_str as the cover page image
         """
-        #import shutil
+        # import shutil
         assert len(self.opf.spine) == 0
 
-        #src = "%s/%s" % (self.IMAGES, "cover.jpg")
-        #filename = os.path.join("OEBPS", self.IMAGES, "cover.jpg")
-        #self.zip.writestr(filename, image_str)
+        # src = "%s/%s" % (self.IMAGES, "cover.jpg")
+        # filename = os.path.join("OEBPS", self.IMAGES, "cover.jpg")
+        # self.zip.writestr(filename, image_str)
 
         src = self.add_image("cover.jpg", image_str, "cover-image")
 
@@ -470,18 +497,32 @@ class EPub(object): # pylint: disable=too-many-instance-attributes
         xfile.c_id = "cover"
         xfile.title = "Cover Page"
         xfile.content = "<p><img alt=\"%s\" src=\"%s\" /></p>" % \
-                (self.title, src)
-        xfile.headers.append(("style", "img { max-width: 100%; }",
-                              {"type":"text/css",}))
+            (self.title, src)
+        header_style = """
+        html, body
+        {
+            width: 100%;
+            height: 100%;
+            margin: 0;
+            padding: 0;
+            overflow: hidden;
+        }
+        img {
+            width: 100%;
+            max-width: 100%;
+        }
+        """
+        xfile.headers.append(("style", header_style,
+                              {"type": "text/css", }))
         self.add_chapter(xfile, "no")
 
     def add_style(self, strstyle, content=""):
         """
         add a style to ebook, calling the correct help to add a file or string
         """
-        if content: # str is name of css file to use
+        if content:  # str is name of css file to use
             src = self.add_style_str(content, strstyle)
-        else: # str is filename of actual css file
+        else:  # str is filename of actual css file
             src = self.add_style_file(strstyle)
 
         self.opf.add_manifest(sluggify(src), src, "text/css")
@@ -503,13 +544,14 @@ class EPub(object): # pylint: disable=too-many-instance-attributes
         self.zip.write(style_filename, filename)
         return "%s/%s" % (self.STYLES, name)
 
+
 def test_main():
     """
     test unit main()
     """
-    from mkcoverpg import MakeCoverPageStr
-    #from SOLBook import SOLStory
-    #s = SOLStory(56030)
+    from .mkcoverpg import MakeCoverPageStr
+    # from SOLBook import SOLStory
+    # s = SOLStory(56030)
     with open('dnstairs.jpg', 'r') as inp:
         img = inp.read()
     ebook = EPub("test epub", "author", "test")
@@ -537,4 +579,3 @@ def test_main():
 if __name__ == "__main__":
     import sys
     sys.exit(test_main())
-
